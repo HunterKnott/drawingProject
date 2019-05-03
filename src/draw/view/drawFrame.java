9d82810f0eceb58b2359ea20bd4f4ac44168ab89
Hunter <hunterknott00@gmail.com>
2019-05-03 13:16:04 -0600

    Made DrawPanel

--- src/draw/view/drawFrame.java
+++ src/draw/view/drawFrame.java
@@ -1,5 +1,25 @@
 package draw.view;
 
-public class DrawFrame {
+import javax.swing.JFrame;
+import draw.controller.DrawController;
 
+public class DrawFrame extends JFrame
+{
+	private DrawController app;
+	
+	public DrawFrame(DrawController app)
+	{
+		super();
+		this.app = app;
+		setupFrame();
+	}
+	
+	private void setupFrame()
+	{
+		this.setTitle("Java Drawing");
+		this.setSize(1000, 800);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
+		this.setVisible(true);
+	}
 }
